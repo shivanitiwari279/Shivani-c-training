54e4913802a7084ac7b3a48e8693d16ec345aa7e
shivanitiwari359 <shivanitiwari@gmail.com>
2025-09-22 14:41:19 +0530

    Updated code

--- C/array.c/code6.c
+++ C/array.c/code6.c
@@ -31,35 +31,20 @@
 
 //PALINDROME OR NOT
 
-// #include<stdio.h>
-// int main(){
-//     int arr[5]={1,2,3,2};
+// #include <stdio.h>
+
+// int main() {
+//     int arr[5] = {1, 2, 3, 4 ,5};
 //     int i;
-//     for (int i = 0; i < 5/2; i++)
-//     {
-//         if (arr[i]!=arr[5-i-1])
-//         {
-//             printf(" Not Palindrome! \n");
+
+//     for(i = 0; i < 5/2; i++) {
+//         if(arr[i] != arr[5 - i - 1]) {
+//             printf("Not Palindrome\n");
+//             return 0;   
 //         }
-        
 //     }
-       
-//     printf(" Palindrom! \n");
+
+//     printf("Palindrome\n");  
+//     return 0;
 // }
 
-#include <stdio.h>
-
-int main() {
-    int arr[5] = {1, 2, 3, 4 ,5};
-    int i;
-
-    for(i = 0; i < 5/2; i++) {
-        if(arr[i] != arr[5 - i - 1]) {
-            printf("Not Palindrome\n");
-            return 0;   
-        }
-    }
-
-    printf("Palindrome\n");  
-    return 0;
-}
